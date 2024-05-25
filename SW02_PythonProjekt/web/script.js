d15b915d11729316a2794d9d277a41c4e3cb3bce
ganjire <101351471+ganjire@users.noreply.github.com>
2024-05-26 00:56:44 +0200

    Add files via upload

---
+++ SW02_PythonProjekt/web/script.js
@@ -0,0 +1,9 @@
+function inputChanged(event) {
+    fetch('/sum?n=' + event.target.value )
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            document.getElementById("result").value = data["sum"];
+        });
+}
+
